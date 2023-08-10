218caf9a1f6aceca2220d2e6a4cc7d3f63c2331e
nachoprogram <pignattanacho@gmail.com>
2023-08-10 18:30:21 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,9 +1,11 @@
 
+function alerta_boton(){
+    alert("Hola");
+}
 function alerta_div(evento){
     alert("Hola! Soy el div");
 }
 
 let div = document.getElementById("div");
-div.addEventListener("click", function(){
-    alert("Hola! Soy el div");
-});
+div.addEventListener("click", alert("Hola! Soy el div"));
+  p.onclick = alerta_div; 
